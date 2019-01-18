04c7a6db8c08592d6868ddb0bca4e6137dccbad6
swetha <swethad98761@gmail.com>
2019-01-18 15:59:56 +0530

    KYC.java

--- src/Assignment/KYC.java
+++ src/Assignment/KYC.java
@@ -1,38 +1,68 @@
 package Assignment;
 
-import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.temporal.ChronoUnit;
+
 
 public class KYC {
-    public static void main(String args[]) throws IOException
-    {
-        //Used to read the input
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int testCases=Integer.parseInt(br.readLine());
-        //DateTimeFormatter to print and accept the date of particular format
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        while(testCases>0)
+    //To check whether the currentDate is in between startingDate and endingDate
+    public static boolean isInBetweenDates(LocalDate startingDate,LocalDate endingDate,LocalDate currentDate) {
+        //compareTo is used to compare 2 dates
+        if(currentDate.compareTo(startingDate) * currentDate.compareTo(endingDate) <0)
         {
-            String input[]=br.readLine().split(" ");
-            LocalDate signUpDate=LocalDate.parse(input[0],formatter);
-            LocalDate currentDate=LocalDate.parse(input[1],formatter);
-            //Trying to get the closest Anniversary by adding years
-            LocalDate closestAnniversary=signUpDate.plusYears(currentDate.getYear()-signUpDate.getYear());
-            LocalDate lowerBound=closestAnniversary.minusDays(30);
-            LocalDate upperBound=closestAnniversary.plusDays(30);
-            //compareTo is used to compare 2 dates
-            if(currentDate.compareTo(signUpDate)<0)
-            {
-                System.out.println("No range");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+    public static LocalDate getClosestDate(LocalDate anniversaryDateOfCurrentYear,LocalDate anniversaryDateOfPreviousYear,LocalDate currentDate) {
+        if(ChronoUnit.DAYS.between(currentDate,anniversaryDateOfCurrentYear) > 30)
+        {
+            return anniversaryDateOfPreviousYear;
+        }
+        else if(Math.abs(ChronoUnit.DAYS.between(currentDate,anniversaryDateOfCurrentYear)) >= Math.abs(ChronoUnit.DAYS.between(anniversaryDateOfPreviousYear,currentDate)))
+        {
+            return anniversaryDateOfPreviousYear;
+        }
+        else
+        {
+            return anniversaryDateOfCurrentYear;
+        }
+
+    }
+    public static void main(String[] args) {
+        //Used to read the input
+        Scanner scanner = new Scanner(System.in);
+        int noOfTestCases = scanner.nextInt();
+        for (int testCase = 0; testCase < noOfTestCases; testCase++) {
+            String signUpDateInString = scanner.next();
+            String currentDateInString = scanner.next();
+            //DateTimeFormatter to print and accept the date of particular format
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate signUpDate = LocalDate.parse(signUpDateInString, formatter);
+            LocalDate currentDate = LocalDate.parse(currentDateInString, formatter);
+            if (signUpDate.getYear() >= currentDate.getYear() || signUpDate.plusYears(1).minusDays(30).compareTo(currentDate)>0) {
+                System.out.println("No Range");
             }
             else {
-                if (currentDate.compareTo(lowerBound) * currentDate.compareTo(upperBound) < 0) {
-                    upperBound = currentDate;
+                //Trying to get the closest Anniversary by adding years
+                LocalDate anniversaryDateOfCurrentYear = signUpDate.plusYears(currentDate.getYear() - signUpDate.getYear());
+                LocalDate anniversaryDateOfPreviousYear= anniversaryDateOfCurrentYear.minusYears(1);
+                LocalDate closestAnniversaryDate=getClosestDate(anniversaryDateOfCurrentYear,anniversaryDateOfPreviousYear,currentDate);
+                LocalDate startingDate = closestAnniversaryDate.minusDays(30);
+                LocalDate endingDate = closestAnniversaryDate.plusDays(30);
+                if(isInBetweenDates(startingDate,endingDate,currentDate)) {
+                    System.out.println(formatter.format(startingDate)+" "+formatter.format(currentDate));
                 }
-                System.out.println(formatter.format(lowerBound) + " " + formatter.format(upperBound));
+                else {
+                    System.out.println(formatter.format(startingDate)+" "+formatter.format(endingDate));
+                }
             }
-            testCases--;
         }
     }
 }
